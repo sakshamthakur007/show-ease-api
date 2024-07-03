6aa184a6961f5d90ab3f54524f0601efc936b41f
Saksham Thakur <103363770+sakshamthakur007@users.noreply.github.com>
2024-07-03 21:17:52 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,11 +4,12 @@ const connectDB = require('./dbConnection')
 const Ticket = require('./schema');
 const cors = require("cors");
 
-app.use(cors(
-    {
-        origin:'https://shoe-ease-frontend.vercel.app'
-    }
-));
+app.use(cors({
+    origin: 'https://shoe-ease-frontend.vercel.app', // Note: No trailing slash
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed methods
+    allowedHeaders: ['Content-Type', 'Authorization'], // Allowed headers
+    credentials: true // If you need to include cookies in the requests
+}));
 //Middleware for parsing Json
 app.use(express.json());
 //Connecting to Database
