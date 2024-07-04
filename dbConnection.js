6f053f26710d3df6d87d0ed6447a1503ae536f6c
Saksham Thakur <103363770+sakshamthakur007@users.noreply.github.com>
2024-07-04 17:29:42 +0530

    Update dbConnection.js

--- dbConnection.js
+++ dbConnection.js
@@ -2,7 +2,10 @@ const mongoose = require('mongoose');
 
 const connectDB = async() => {
   try{
-    await mongoose.connect('mongodb+srv://sakshamthakur157:010101@cluster0.dkbufqg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
+    await mongoose.connect('mongodb+srv://sakshamthakur157:010101@cluster0.dkbufqg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
     console.log('Connected to Database')}
   catch(error){
     console.log(error)}
